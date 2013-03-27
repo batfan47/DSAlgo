3024d4fed63c6420a66c5210045fcf1ef6d6e04b
batfan47 <batfan47@gmail.com>
2013-03-27 08:02:08 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#define higher(a, b) (a > b ? a : b)
 using namespace::std;
 typedef long long element;
 typedef unsigned long counter;
 typedef pair<counter, counter> cpair;
 
-element kadane (element terms[], counter count, counter &startloc, counter & endloc)
+element kadane (element terms[], counter count, counter &startloc, counter &endloc)
 {
   if (!count) return 0;
-  element max = terms[0], currMax = terms[0];
+  element max = terms[0], cmax = terms[0];
   counter cstart = 0, cend = 0;
   for (counter i = 1; i < count; ++i) {
-    if (currMax + terms[i] > terms[i]) currMax += terms[i];
-    else {
-      currMax = terms[i];
+    if (terms[i] > cmax + terms[i]) {
+      cmax = terms[i];
       cstart = i;
     }
+    else cmax += terms[i];
     cend = i;
-    if (currMax > max) {
-      max = currMax += terms[i];
+    if (cmax > max) {
+      max = cmax;
       startloc = cstart;
       endloc = cend;
     }
